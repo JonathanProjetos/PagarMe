f80dab516911d9690af33bfdb2696094f3c74b2e
Jonathan Santos <jonathan01347@gmail.com>
2023-07-06 22:56:25 -0300

    feat(seeder)add seerder for test user.

---
+++ src/database/seeders/20230706160952-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'John Doe',
+        email: 'test@test.com',
+        hashPassword: '$2a$10$ct.1ZS11o6b10aASci3sIe826wQWN/6Rxl.e0XT.y48ZwWGuE3Rt.', // 123456
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {}
+};

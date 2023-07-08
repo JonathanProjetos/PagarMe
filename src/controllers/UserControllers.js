a28a245f5d0a66e776f473abfc6286139f1464f6
Jonathan Santos <jonathan01347@gmail.com>
2023-07-07 23:01:10 -0300

    feat(UserController)added function for register.

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -6,6 +6,12 @@ const UserController = {
     const user = await UserServices.Login(body);
     res.status(200).json(user);
   },
+
+  Register: async(req, res) => {
+    const { body } = req;
+    const user = await UserServices.Register(body);
+    res.status(201).json(user);
+  }
 };
 
 module.exports = UserController;

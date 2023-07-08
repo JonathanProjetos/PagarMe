04b8eaafb5073604fa72d1a7ac2301dfd303f8ba
Jonathan Santos <jonathan01347@gmail.com>
2023-07-07 22:42:13 -0300

    feat(UserService)Added validation for input data. Added register function.

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,28 +1,43 @@
 const { User } = require('../database/models');
 const bycrypt = require('bcryptjs');
 const jwt = require('../middlewares/token');
+const { validateBodyLogin, validateBodyRegister } = require('../helpers/joiUser');
 
 
 const UserService = {
   Login: async (body) => {
-    const { email, password } = body;
+    const data = validateBodyLogin(body);
 
-    console.log(email);
+    const { email, password } = data;
+
     const checkUser = await User.findOne({ where: { email } });
 
     if (!checkUser) throw new Error('400|User not found');
 
-    const passwordMatch = await bycrypt.compareSync(password, checkUser.hashPassword);
-
-    if (!passwordMatch) throw new Error('400|Password does not match');
+    if (!bycrypt.compareSync(password, checkUser.hashPassword)){
+      throw new Error('401|Password does not match');
+    }
 
     const createToken = jwt.generateToken(email);
 
     return { token: createToken };
   },
 
-  Registrer: async (body) => {
-  
+  Register: async (body) => {
+
+    const data = validateBodyRegister(body);
+
+    const { email, password } = data;
+
+    const checkUser = await User.findOne({ where: { email } });
+
+    if (checkUser) throw new Error('409|User already registered');
+
+    const hashPassword = bycrypt.hashSync(password, 10);
+
+    await User.create({ email, hashPassword });
+
+    return { message: 'User created successfully' };
   }
 }
 

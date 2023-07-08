f7d076849d37150fa79ee9d5777c51b53338b1af
Jonathan Santos <jonathan01347@gmail.com>
2023-07-07 23:02:24 -0300

    feat(app)rendered transaction route and documentation

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 const UserRouter = require('./router/UserRouter');
+const TransactionRouter = require('./router/TransactionRoute');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./doc/swagger.json');
 
 app.use(express.json());
 
 app.use('/', UserRouter);
+app.use('/', TransactionRouter);
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err, _req, res, _next) => {
   const[status, message] = err.message.split('|');

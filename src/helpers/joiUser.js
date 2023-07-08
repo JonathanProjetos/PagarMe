3e4a1f65f57e5a4dd9cdf36b110679349bf21571
Jonathan Santos <jonathan01347@gmail.com>
2023-07-07 22:53:05 -0300

    feat(joiUser)added schema joi for validate input data

---
+++ src/helpers/joiUser.js
@@ -0,0 +1,59 @@
+const Joi = require('joi');
+
+const validateBodyLogin = (body) => {
+  const userSchema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.empty': '400|The "email" field cannot be empty.',
+      'any.required': '400|The "email" field is mandatory.',
+      'string.email': '400|The "email" must be in the format test@test.com.',
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': '400|The "password" field cannot be empty.',
+      'any.required': '400|The "password" field is mandatory.',
+      'string.min': '400|The "password" field must be at least 6 characters.',
+    }),
+  });
+
+  const { error, value } = userSchema.validate(body);
+
+  if (error) {
+    throw error;
+  }
+  return value;
+}
+
+
+const validateBodyRegister = (body) => {
+
+  const registerSchema = Joi.object({
+    name: Joi.string().messages({
+      'string.base': '400|The "name" field must be a string.',
+    }),
+    lastName: Joi.string().messages({
+      'string.base': '400|The "lastName" field must be a string.',
+    }),
+    email: Joi.string().email().required().messages({
+      'string.empty': '400|The "email" field cannot be empty.',
+      'any.required': '400|The "email" field is mandatory.',
+      'string.email': '400|The "email" must be in the format test@test.com.',
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': '400|The "password" field cannot be empty.',
+      'any.required': '400|The "password" field is mandatory.',
+      'string.min': '400|The "password" field must be at least 6 characters.',
+    }),
+  })
+
+  const { error, value } = registerSchema.validate(body);
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  return value;
+}
+
+module.exports = {
+  validateBodyLogin,
+  validateBodyRegister,
+};

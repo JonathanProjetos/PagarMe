e9f715ae94cbf4fc010aa1f7548ff357aca7fc04
Jonathan Santos <jonathan01347@gmail.com>
2023-07-10 22:03:42 -0300

    fix(app)print of the errors thrown.

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ app.use('/', TransactionRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err, _req, res, _next) => {
+  console.error(err);
   const[status, message] = err.message.split('|');
   res.status(Number(status)).json({ error: message });
 })
-
 
 module.exports = app;

1fe3b9455e394bc4a6f923293cec47ca88fdcd0a
Jonathan Santos <jonathan01347@gmail.com>
2023-07-10 22:05:36 -0300

    feat(TransactionControllers)added function to create transaction in service.

--- src/services/TransectionService.js
+++ src/services/TransectionService.js
@@ -1,17 +1,51 @@
 const { User, Transaction } = require('../database/models');
+const joiTransaction = require('../helpers/joiTransaction');
+const sliceNumberCard = require('../helpers/sliceNumberCard');
+const cardExpirationDateFormat = require('../helpers/expiredCardDate');
+const cvv = require('../helpers/cvvFormat');
 
 const TransactionService = {
 
-  getAll: async (email) => {
+  getOne: async (email) => {
     const user = await User.findOne({ where: { email } });
-    if(!user) throw new Error('400|User not found');
-    const transactions = await User.findAll({ 
+
+    if(!user) throw new Error('401|Unauthorized user');
+
+    const transactions = await User.findOne({ 
       where: { id: user.id },
+      attributes: { exclude: ['hashPassword'] },
       include: { model: Transaction, as: 'transactions' }, 
     });
+    
     return transactions;
   },
 
+  create: async (body, email) => {
+    const data = joiTransaction(body);
+
+    const sliceNumber = sliceNumberCard(data.cardNumber);
+
+    const cardExpiration = cardExpirationDateFormat(data.cardExpirationDate);
+
+    const cardCvv = cvv(data.cardCvv);
+
+    const user = await User.findOne({ where: { email } });
+
+    if(!user) throw new Error('400|User not found');
+
+    await Transaction.create({
+      userId: user.id,
+      amount: data.amount,
+      description: data.description,
+      paymentMethod: data.paymentMethod,
+      cardNumber: sliceNumber,
+      cardHolderName: data.cardHolderName,
+      cardExpirationDate: cardExpiration,
+      cardCvv,
+    });
+
+    return { message: 'Transaction created successfully'}
+  }
 };
 
 module.exports = TransactionService;

0530965d3eb2b3fbd2965c2e0b84c852d7cfc3df
Jonathan Santos <jonathan01347@gmail.com>
2023-07-10 22:08:05 -0300

    feat(TransactionRouter)added route for create transaction.

--- src/router/TransactionRoute.js
+++ src/router/TransactionRoute.js
@@ -3,6 +3,7 @@ const TransactionRouter = express.Router();
 const TransactionController = require('../controllers/TransactionControllers');
 const { verifyToken } = require('../middlewares/decodeToken');
 
-TransactionRouter.get('/transaction', verifyToken, TransactionController.getAll);
+TransactionRouter.get('/transaction', verifyToken, TransactionController.getOne);
+TransactionRouter.post('/transaction', verifyToken, TransactionController.create);
 
 module.exports = TransactionRouter;

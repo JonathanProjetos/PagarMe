f18d1e073ac29d010c133e5a25c7ec323250c68a
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 19:32:44 -0300

    test(mock)add mock to mockDataTransaction

--- src/tests/mocks/mockDataTransaction.js
+++ src/tests/mocks/mockDataTransaction.js
@@ -1,6 +1,6 @@
 const dataReponse = {
 		id: 1,
-		name: "John Doe",
+		name: "m3gan eu sou robô",
 		lastName: null,
 		email: "test@test.com",
 		hashPassword: "$2a$10$ct.1ZS11o6b10aASci3sIe826wQWN/6Rxl.e0XT.y48ZwWGuE3Rt.",
@@ -11,11 +11,37 @@ const dataReponse = {
 				description: "Smartband XYZ 3.0",
 				paymentMethod: "debit_card",
 				cardNumber: 2345678912345678,
-				cardHolderName: "John Doe",
+				cardHolderName: "m3gan eu sou robô",
 				cardExpirationDate: "2021-12",
 				cardCvv: 123,
-			}
+			},
 		]
 };
 
-module.exports = dataReponse;
+const dataReponsePayable = {
+	id: 1,
+	name: "m3gan eu sou robô",
+	lastName: null,
+	email: "test@test.com",
+	hashPassword: "$2a$10$ct.1ZS11o6b10aASci3sIe826wQWN/6Rxl.e0XT.y48ZwWGuE3Rt.",
+	createdAt: "2023-07-07",
+	transactions: [
+		{
+			amount: 100.00,
+			description: "Smartband XYZ 3.0",
+			paymentMethod: "debit_card",
+			cardNumber: 2345678912345678,
+			cardHolderName: "m3gan eu sou robô",
+			cardExpirationDate: "2021-12",
+			cardCvv: 123,
+			payable: {
+				transactionId: 1,
+				status: "paid",
+				paymentDate: "2023-07-07",
+				amount: 97.00
+			}
+		},
+	]
+};
+
+module.exports = {dataReponse, dataReponsePayable};

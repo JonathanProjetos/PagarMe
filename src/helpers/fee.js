1115b8bc6ad1c11489a0afd81b936b577b6d5d29
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:32:52 -0300

    fix(fee)eslint bug fix

--- src/helpers/fee.js
+++ src/helpers/fee.js
@@ -1,20 +1,22 @@
-
 // Está função define a taxa de acordo com o método de pagamento. Podendo variar entre 3% e 5%.
+
 const fee = (body) => {
-
-  if(!body.paymentMethod) throw new Error('404|paymentMethod is required');
-
-  if(body.paymentMethod === 'debit_card') {
-    const fee = Number(body.amount) * 0.03;
-    body.amount = Number(body.amount) - fee;
-    return body;    
+  if (!body.paymentMethod) {
+    throw new Error('400|paymentMethod is required');
   }
 
-  if(body.paymentMethod === 'credit_card') {
-    const fee = Number(body.amount) * 0.05;
-    body.amount = Number(body.amount) - fee;
+  if (body.paymentMethod === 'debit_card') {
+    const feeDebit = Number(body.amount) * 0.03;
+    body.amount = Number(body.amount) - feeDebit;
     return body;
   }
+
+  if (body.paymentMethod === 'credit_card') {
+    const feeCredit = Number(body.amount) * 0.05;
+    body.amount = Number(body.amount) - feeCredit;
+    return body;
+  }
+  return true;
 };
 
 module.exports = fee;

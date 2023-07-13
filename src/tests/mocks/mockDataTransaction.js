9007df855e2826d694b97a82b9ffe6745ed56cd1
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:44:30 -0300

    test(mock)added mock to test.

--- src/tests/mocks/mockDataTransaction.js
+++ src/tests/mocks/mockDataTransaction.js
@@ -4,10 +4,10 @@ const dataReponse = {
 		lastName: null,
 		email: "test@test.com",
 		hashPassword: "$2a$10$ct.1ZS11o6b10aASci3sIe826wQWN/6Rxl.e0XT.y48ZwWGuE3Rt.",
-		createdAt: "2023-07-07T16:34:51.000Z",
+		createdAt: "2023-07-07",
 		transactions: [
 			{
-				amount: "100.00",
+				amount: 100.00,
 				description: "Smartband XYZ 3.0",
 				paymentMethod: "debit_card",
 				cardNumber: 2345678912345678,

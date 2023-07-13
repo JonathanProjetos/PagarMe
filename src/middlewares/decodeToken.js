65866cf81c923055412b8175f7a7119c29467ad7
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:39:35 -0300

    fix(decodeToken)eslint bug fix

--- src/middlewares/decodeToken.js
+++ src/middlewares/decodeToken.js
@@ -1,18 +1,15 @@
 const token = require('./token');
 
 module.exports = {
-  
   // Middleware que verifica se o token é válido.
   verifyToken: (req, _res, next) => {
     try {
       const { authorization } = req.headers;
       const filterBearerSwagger = authorization.split(' ').pop('Bearer');
       const dados = token.validateToken(filterBearerSwagger);
-      console.log('middleware',dados);
       req.email = dados.email;
       next();
     } catch (err) {
-      console.error(err);
       throw new Error('404|Token not found');
     }
   },

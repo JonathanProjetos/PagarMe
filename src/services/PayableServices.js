87f6cff224e888155cac9cba3efe30ca2dea3133
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:41:37 -0300

    fix(PayabledServices)eslint bug fix

--- src/services/PayableServices.js
+++ src/services/PayableServices.js
@@ -4,27 +4,29 @@ const sumPayable = require('../helpers/sumPayable');
 
 const PayableServices = {
   getBalance: async (query, email) => {
+    if (!query) throw new Error('400|Status is required');
+
     // Faz a validação do status da query
-    const status = joiPayable({ status: query })
+    const status = joiPayable({ status: query });
 
     // Resgato o usuário no banco de dados com base no email.
     const user = await User.findOne({ where: { email } });
 
     // Caso não exista, retorna um erro.
-    if(!user) throw new Error('401|Unauthorized user');
+    if (!user) throw new Error('401|Unauthorized user');
 
     // Resgato as transações do usuário.
-    const transactions = await User.findOne({ 
+    const transactions = await User.findOne({
       where: { id: user.id },
       attributes: { exclude: ['hashPassword'] },
-      include: { 
-        model: Transaction, 
+      include: {
+        model: Transaction,
         as: 'transactions',
         include: {
           model: Payable,
           as: 'payable',
-          attributes: { exclude: ['id'] }
-        }
+          attributes: { exclude: ['id'] },
+        },
       },
 
     });

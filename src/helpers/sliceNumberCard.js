637852de3a5df93c637cff5a33d2e96351e521b6
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:36:38 -0300

    fix(sliceNumberCard)eslint bug fix

--- src/helpers/sliceNumberCard.js
+++ src/helpers/sliceNumberCard.js
@@ -1,18 +1,16 @@
 const sliceNumber = (number) => {
-
-  //Recebo o numero do cartao e converto para string
+  // Recebo o numero do cartao e converto para string.
   const numberString = number.toString();
-  
-  //verifico se o numero tem 16 digitos
-  if(numberString.split('').length !== 16){
-    throw new Error('404|The number must have 16 digits');
+
+  // Verifica se o número tem 16 digitos.
+  if (numberString.split('').length !== 16) {
+    throw new Error('400|The number must have 16 digits');
   }
 
-  //pego os 4 ultimos digitos do numero
-  const sliceNumber = numberString.slice(-4);
+  // Pega os 4 últimos dígitos do número.
+  const slice = numberString.slice(-4);
 
-  return Number(sliceNumber);
+  return Number(slice);
+};
 
-}
-
-module.exports = sliceNumber
+module.exports = sliceNumber;

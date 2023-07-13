e6ebc48f78512dbcc199f29b47b373f2fed9e7ef
Jonathan Santos <jonathan01347@gmail.com>
2023-07-12 22:44:39 -0300

    feat(PayableControllers)Added function in controller.

---
+++ src/controllers/PayableControllers.js
@@ -0,0 +1,12 @@
+const PayableServices = require('../services/PayableServices');
+
+const PayableController = {
+  getBalance: async (req, res) => {
+    const { email } = req;
+    const { status } = req.query;
+    const balance = await PayableServices.getBalance(status, email);
+    res.status(200).json(balance);
+  }
+};
+
+module.exports = PayableController;

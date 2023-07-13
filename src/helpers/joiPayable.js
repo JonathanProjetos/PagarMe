d94dac498c140323b90932d5a40eb0cd4906a304
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:35:44 -0300

    fix(joiPayable)eslint bug fix

--- src/helpers/joiPayable.js
+++ src/helpers/joiPayable.js
@@ -1,12 +1,13 @@
 const Joi = require('joi');
 
-// Está função esta validando o dados de entrada se é paid ou waiting_founds. Qualquer outro valor será retornado um erro.
+// Está função esta validando o dados de entrada se é paid ou waiting_founds.
+// Qualquer outro valor será retornado um erro.
 const validatePayable = (body) => {
   const payableSchema = Joi.object({
-    status: Joi.string().valid("paid", "waiting_funds").required().messages({
-      'string.empty': '404|The "status_payment" field cannot be empty.',
-      'any.required': '404|The "status_payment" field is mandatory.',
-      'any.only': '404|status_payment must be paid or waiting_funds'
+    status: Joi.string().valid('paid', 'waiting_funds').required().messages({
+      'string.empty': '400|The "status_payment" field cannot be empty.',
+      'any.required': '400|The "status_payment" field is mandatory.',
+      'any.only': '400|status_payment must be paid or waiting_funds',
     }),
   });
 
@@ -16,8 +17,6 @@ const validatePayable = (body) => {
     throw error;
   }
   return value;
-}
-
-
+};
 
 module.exports = validatePayable;

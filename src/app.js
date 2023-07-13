f448fc6a0530c8ebfc81e084cb4189618d105b85
Jonathan Santos <jonathan01347@gmail.com>
2023-07-12 22:43:13 -0300

    feat(app)rendered a new route payable

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const UserRouter = require('./router/UserRouter');
 const TransactionRouter = require('./router/TransactionRoute');
+const PayableRouter = require('./router/PayableRoute');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./doc/swagger.json');
 
@@ -10,13 +11,16 @@ app.use(express.json());
 
 app.use('/', UserRouter);
 app.use('/', TransactionRouter);
+app.use('/', PayableRouter);
 
+// endpoint para acessar a documentação da API
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// middleware para tratamento de erros.Todos os erros são lançados com o formato 'status|message'.
 app.use((err, _req, res, _next) => {
   console.error(err);
   const[status, message] = err.message.split('|');
   res.status(Number(status)).json({ error: message });
-})
+});
 
 module.exports = app;

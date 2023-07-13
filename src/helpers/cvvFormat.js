d4b5446f5e71b4ca49719c2747eebc4ad3684f9a
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:32:03 -0300

    fix(cvvFormat)eslint bug fix

--- src/helpers/cvvFormat.js
+++ src/helpers/cvvFormat.js
@@ -1,9 +1,10 @@
+// Está função recebe o cvv do cartão de crédito ou débito. Caso o cvv seja inválido,
+// retorna um erro personalizado no formato 'status|message'.
 
-//Está função recebe o cvv do cartão de crédito ou débito. Caso o cvv seja inválido, retorna um erro personalizado no formato 'status|message'.
 const cvvFormat = (data) => {
   const cvv = data.toString();
-  if(cvv.length !== 3) throw new Error('400|Invalid card cvv');
+  if (cvv.length !== 3) throw new Error('400|Invalid card cvv');
   return Number(cvv);
-} 
+};
 
 module.exports = cvvFormat;

2f4245f2906b3a8cf24cf7d0adbb4fdd3f46001d
Jonathan Santos <jonathan01347@gmail.com>
2023-07-13 15:36:19 -0300

    fix(joiUser)eslint bug fix

--- src/helpers/joiUser.js
+++ src/helpers/joiUser.js
@@ -1,17 +1,20 @@
 const Joi = require('joi');
 
-// Está função esta validando os dados de entrada para login. Caso surja alguma incompatibilidade, retorno o erro personalizado no formato 'status|message'.
+// Está função esta validando os dados de entrada para login.
+// Caso surja alguma incompatibilidade, retorno o erro personalizado no formato 'status|message'.
 const validateBodyLogin = (body) => {
   const userSchema = Joi.object({
     email: Joi.string().email().required().messages({
       'string.empty': '400|The "email" field cannot be empty.',
       'any.required': '400|The "email" field is mandatory.',
       'string.email': '400|The "email" must be in the format test@test.com.',
+      'string.base': '400|The "email" field must be a string',
     }),
     password: Joi.string().min(6).required().messages({
       'string.empty': '400|The "password" field cannot be empty.',
       'any.required': '400|The "password" field is mandatory.',
       'string.min': '400|The "password" field must be at least 6 characters.',
+      'string.base': '400|The "password" field must be a string',
     }),
   });
 
@@ -21,11 +24,11 @@ const validateBodyLogin = (body) => {
     throw error;
   }
   return value;
-}
+};
 
-// Está função esta validando os dados de cadastro de usuário. Caso surja alguma incompatibilidade, retorno o erro personalizado no formato 'status|message'.
+// Está função esta validando os dados de cadastro de usuário.
+// Caso surja alguma incompatibilidade, retorno o erro personalizado no formato 'status|message'.
 const validateBodyRegister = (body) => {
-
   const registerSchema = Joi.object({
     name: Joi.string().messages({
       'string.base': '400|The "name" field must be a string.',
@@ -37,22 +40,24 @@ const validateBodyRegister = (body) => {
       'string.empty': '400|The "email" field cannot be empty.',
       'any.required': '400|The "email" field is mandatory.',
       'string.email': '400|The "email" must be in the format test@test.com.',
+      'string.base': '400|The "email" field must be a string',
     }),
     password: Joi.string().min(6).required().messages({
       'string.empty': '400|The "password" field cannot be empty.',
       'any.required': '400|The "password" field is mandatory.',
       'string.min': '400|The "password" field must be at least 6 characters.',
+      'string.base': '400|The "password" field must be a string',
     }),
-  })
+  });
 
   const { error, value } = registerSchema.validate(body);
 
   if (error) {
-    console.log(error);
     throw error;
   }
+
   return value;
-}
+};
 
 module.exports = {
   validateBodyLogin,
